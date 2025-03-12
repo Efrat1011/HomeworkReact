b8d0bf61b3ef888ae56052dea837b7a51f23c8ec
Ifrat <ifrat1998@gmail.com>
2025-03-12 13:32:33 +0500

    Login Box

---
+++ d2-w27-hw/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LoginBox from './component/LoginBox'
+
+function App() {
+  
+
+  return (
+    <>
+      <LoginBox/>
+    </>
+  )
+}
+
+export default App
